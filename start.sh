474364d9b2e0b03bba452f04f709de97ea84bc2a
LabDig <andre.coelho@ufv.br>
2019-09-13 19:11:55 -0200

    A

--- start.sh
+++
@@ -1,9 +0,0 @@
-#/!bin/bash
-vncserver -geometry 1280x640 -depth 16
-config-pin P9.11 uart
-config-pin P9.13 uart
-config-pin P9.24 uart
-config-pin P9.26 uart
-config-pin P9.21 uart
-config-pin P9.22 uart
-
